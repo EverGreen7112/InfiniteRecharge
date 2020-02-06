d0c6897a5283a1d20ff6aec7e35d4c63114dc5cb
ToMalki <malki.tom@gmail.com>
2020-02-06 22:04:22 +0200

    feat(new feature):<added chassis>
    added a new chassis object with new pid controllers and pid coments

---
+++ src/main/java/com/evergreen/robot/wpilib/commands/MoveChassisTo.java
@@ -0,0 +1,13 @@
+package com.evergreen.robot.wpilib.commands;
+
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+
+/**
+ * MoveChassisTo
+ */
+public class MoveChassisTo extends PIDCommand {
+
+    public MoveChassisTo(double distance) {
+        super
+    }
+}
