fc5a7be38ebb3131279ada7e097c686c536f5b88
Atai AMbus <atayambus@gmail.com>
2020-02-19 20:08:03 +0200

    upload()

--- src/main/java/com/evergreen/robot/wpilib/Robot.java
+++ src/main/java/com/evergreen/robot/wpilib/Robot.java
@@ -1,22 +1,30 @@
 package com.evergreen.robot.wpilib;
 
+import java.util.Set;
+
 import com.evergreen.everlib.structure.Tree;
 import com.evergreen.robot.RobotMap.ButtonPorts;
 import com.evergreen.robot.RobotMap.JoystickPorts;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.evergreen.robot.wpilib.subsystem.Shooter;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * Robot
  */
-public class Robot extends Tree {
-    
+public class Robot extends TimedRobot {
+    boolean m_activate = true;
     boolean m_lifting = false;
 
     //Creates JS objects
@@ -25,90 +33,54 @@ public class Robot extends Tree {
     
 
     @Override
-    protected void autoConfig() {
-        Autonomous.getInstance().schedule();
+    public void testInit() {
+        // CommandScheduler.getInstance().run();
+        // new JoystickButton(operatorJS, 1).whenPressed(Rolletta.getInstance().lift());
+        // new JoystickButton(operatorJS, 3).whenPressed(new PrintCommand("Argh"));
+        // Chassis.getInstance().drive(0.5, 0.5);
+        // Chassis.getInstance().getRightControllerGroup().set(0.7);
+        new JoystickButton(operatorJS, 1).whenPressed(new PrintCommand("hello"));
     }
 
-    @Override
-    protected void bindButtons() {
-        //The robot elevates the hook while the button is held
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSA).whenHeld(Climb.getInstance().m_up());
-        //The robot pulls itself upwards while the button is held
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSB).whenHeld(Climb.getInstance().m_pull());
-
-        //The robot collects Power Cells while the button is held
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSX).whenHeld(Collector.getInstance().collect());
-
-        //The robot passes a Power Cells to the Shooter when the button is pressed
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSY).whenPressed(Storage.getInstance().passByTime());
-        //TODO check which passing command is the best - by time or by sensor
-
-    }
-
-    @Override
-    protected void commandConfig() {
-
-    }
-
-    @Override
-    protected void componentSetup() {
-        Autonomous.getInstance();
-    }
-
-    @Override
-    protected void log() {
-
-    }
-
-    @Override
-    protected void teleopConfig() {
-
-    }
-
-    // boolean start;
-
-    @Override
-    protected void test() {
-        Rolletta.getInstance().lift().schedule();
-    }
+   
 
 
 
     @Override
     public void testPeriodic() {
+    //    if(m_activate){
+    //        Rolletta.getInstance().lift().schedule();
+    //        m_activate =false;
+    //    }
         // Rolletta.getInstance().
         // move(Rolletta.getInstance().isLifting());
         // if (Rolletta.getInstance().isUp() != start)
         
         // else Rolletta.getInstance().m_lifter.set(0);
-
-        SmartDashboard.putBoolean("Up", Rolletta.getInstance().isUp());
-        SmartDashboard.putBoolean("Down", Rolletta.getInstance().isDown());
-        SmartDashboard.putBoolean("lifting", m_lifting);
-        CommandScheduler.getInstance().run();
-
+        // SmartDashboard.putBoolean("Up", Rolletta.getInstance().isUp());
+        // SmartDashboard.putBoolean("Down", Rolletta.getInstance().isDown());
+        // SmartDashboard.putBoolean("lifting", m_lifting);
+        // new PrintCommand("hello").schedule();
+        // CommandScheduler.getInstance().run();
+        CommandBase com =new RunCommand(() -> Chassis .getInstance().drive(0.5, 0.5),Chassis.getInstance());
+       com.schedule();
+       CommandScheduler.getInstance().run();
+       
     }
 
-    @Override
-    protected void whenEnabled() {
-        
-    }
 
 
     @Override
     public void robotPeriodic() {
-        Shooter.getInstance().updatePassDistance();
-        Autonomous.getInstance().update();
-        CommandScheduler.getInstance().run();
+        new PrintCommand("hey").schedule();
     }
-    
     
     @Override
     public void disabledInit() {
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSA).whenPressed(Rolletta.getInstance().m_calibrateGreen());
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSB).whenPressed(Rolletta.getInstance().m_calibrateRed());
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSX).whenPressed(Rolletta.getInstance().m_calibrateBlue());
-        new JoystickButton(operatorJS, ButtonPorts.operatorJSY).whenPressed(Rolletta.getInstance().m_calibrateYellow()); 
+        // new JoystickButton(operatorJS, ButtonPorts.operatorJSA).whenPressed(Rolletta.getInstance().m_calibrateGreen());
+        // new JoystickButton(operatorJS, ButtonPorts.operatorJSB).whenPressed(Rolletta.getInstance().m_calibrateRed());
+        // new JoystickButton(operatorJS, ButtonPorts.operatorJSX).whenPressed(Rolletta.getInstance().m_calibrateBlue());
+        // new JoystickButton(operatorJS, ButtonPorts.operatorJSY).whenPressed(Rolletta.getInstance().m_calibrateYellow()); 
     }
     
     @Override
@@ -124,6 +96,10 @@ public class Robot extends Tree {
     @Override
     public void teleopPeriodic() {
         CommandScheduler.getInstance().run();
+    }
+    @Override
+    public void teleopInit() {
+        
     }
     
 }   
